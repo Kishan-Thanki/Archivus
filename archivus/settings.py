c8509fb8cf56a0935d80768a84413b74ba180480
Kishan Thanki <kishanthankiat20@gmail.com>
2025-06-01 02:54:09 +0530

    Prepare for Render Deployment

--- archivus/settings.py
+++ archivus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b3h1%_(y6_kme@#j@b9^8v)82vy46l*-*d2#&xjfb&n=_1*r!e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
