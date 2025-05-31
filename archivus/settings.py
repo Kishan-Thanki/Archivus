2e21ba034e0b97909c08ec3d6ed648322c9f89fa
Kishan Thanki <kishanthankiat20@gmail.com>
2025-06-01 03:11:18 +0530

    Added Healthz route

--- archivus/settings.py
+++ archivus/settings.py
@@ -18,9 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b3h1%_(y6_kme@#j@b9^8v)82vy46l*-*d2#&xjfb&n=_1*r!e'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- archivus/urls.py
+++ archivus/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+def health_check(request):
+    return HttpResponse("OK")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('healthz', health_check),
 ]
