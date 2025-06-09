e28ae01ef34d09760e872307f13705228291e49a
Kishan Thanki <kishanthankiat20@gmail.com>
2025-06-09 13:20:45 +0530

    Fixed urls.py

--- archivus/urls.py
+++ archivus/urls.py
@@ -18,21 +18,12 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 def health_check(request):
     return HttpResponse("OK", status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('healthz', health_check),
-
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
     path('api/v1/', include('core.urls.v1')),
 ]
