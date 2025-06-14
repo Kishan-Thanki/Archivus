62b41e04844032b24077c69e0ffb22c8eeb34f84
Kishan Thanki <kishanthankiat20@gmail.com>
2025-06-14 17:02:28 +0530

    udpated wsgi.py

--- archivus/wsgi.py
+++ archivus/wsgi.py
@@ -8,8 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import environ
+
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+
+project_root_for_env = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(project_root_for_env / ".env")
+
+DJANGO_ENV = env("DJANGO_ENV", default="prod")
+print(f"🌐 WSGI - DJANGO_ENV loaded: {DJANGO_ENV}")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'archivus.settings')
 
